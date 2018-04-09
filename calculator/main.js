3f7ebc236ca6ae176b0d593569c11935070baaa2
daltonmcgee <30909340+daltonmcgee@users.noreply.github.com>
2018-04-09 17:18:35 -0400

    tomato timer has auto reset and Start overlay.

--- calculator/main.js
+++ calculator/main.js
@@ -11,56 +11,7 @@ var registerA = [],
   number = document.getElementsByClassName("number"),
   operator = document.getElementsByClassName("operator")
 
-document.getElementById("clear-all").addEventListener('click', function() {
-  registerA = [];
-  registerB = [];
-  compareRegister = "";
-  output = 0;
-  document.getElementById("display").innerHTML = "";
-})
-
-document.getElementById("clear-last").addEventListener('click', function() {
-  if (registerB.length > 0) {
-    registerB = []
-    document.getElementById("display").innerHTML = registerA.join("");
-  }
-  else {
-    registerA = [];
-    registerB = [];
-    compareRegister = "";
-    output = 0;
-    document.getElementById("display").innerHTML = "";
-  }
-
-})
-
-for (let x of number) {
-  x.addEventListener("click", function() {
-    if (registerA.length > 10) alert("Number too big")
-    else {
-      if (compareRegister === "") {
-        registerA.push(x.innerHTML);
-        document.getElementById("display").innerHTML = registerA.join("");
-      } else {
-        registerB.push(x.innerHTML);
-        document.getElementById("display").innerHTML = registerB.join("");
-      }
-    }
-  })
-}
-
-for (let x of operator) {
-  x.addEventListener("click", function() {
-    if (registerB.length > 0){
-    }
-    else if (registerA.length > 0) {
-      compareRegister = x.innerHTML;
-      document.getElementById("display").innerHTML = compareRegister
-    }
-  })
-}
-
-document.getElementById("equal").addEventListener("click", function() {
+function getOutput() {
   let number1 = 0,
     number2 = 0;
   if (registerA.length === 0) {
@@ -89,7 +40,58 @@ document.getElementById("equal").addEventListener("click", function() {
       output = number1 / number2
       break;
   }
-  document.getElementById("display").innerHTML = parseFloat(output.toString().slice(0,9));
+  document.getElementById("display").innerHTML = parseFloat(output.toString().slice(0, 9));
   registerA = [output];
   registerB = [];
+}
+
+document.getElementById("clear-all").addEventListener('click', function() {
+  registerA = [];
+  registerB = [];
+  compareRegister = "";
+  output = 0;
+  document.getElementById("display").innerHTML = "";
 })
+
+document.getElementById("clear-last").addEventListener('click', function() {
+  if (registerB.length > 0) {
+    registerB = []
+    document.getElementById("display").innerHTML = registerA.join("");
+  } else {
+    registerA = [];
+    registerB = [];
+    compareRegister = "";
+    output = 0;
+    document.getElementById("display").innerHTML = "";
+  }
+
+})
+
+for (let x of number) {
+  x.addEventListener("click", function() {
+    if (registerA.length > 10) alert("Number too big")
+    else {
+      if (compareRegister === "") {
+        registerA.push(x.innerHTML);
+        document.getElementById("display").innerHTML = registerA.join("");
+      } else {
+        registerB.push(x.innerHTML);
+        document.getElementById("display").innerHTML = registerB.join("");
+      }
+    }
+  })
+}
+
+for (let x of operator) {
+  x.addEventListener("click", function() {
+    if (registerB.length > 0) {
+      getOutput();
+      compareRegister = x.innerHTML;
+    } else if (registerA.length > 0) {
+      compareRegister = x.innerHTML;
+      document.getElementById("display").innerHTML = compareRegister
+    }
+  })
+}
+
+document.getElementById("equal").addEventListener("click", function(){getOutput()})
